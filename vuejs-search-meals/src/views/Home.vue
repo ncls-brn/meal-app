85c44589b1dc0f7dc1bc09fbc3de4815c2215a51
ncls-brn <nicolas.j.baran@gmail.com>
2023-03-05 19:55:40 +0100

    alphabet routes

--- vuejs-search-meals/src/views/Home.vue
+++ vuejs-search-meals/src/views/Home.vue
@@ -1,6 +1,24 @@
  <template>
-    Home 
+    <div class="flex flex-col p-8">
+        
+            <input type= "text" class="rounded border-2 border-gray-200 w-full"
+         placeholder="Search for meals"/>
+        
+        
+    
+    <div class="flex justify-center gap-2 mt-2">
+        <router-link :to="{name: 'byletter', params:{letter}}" v-for="letter of letters" :key="letter">
+            {{ letter }}
+        </router-link>
+    </div>
+</div>
+    
  </template>
-
+ 
  <script setup>
+ import { computed } from 'vue';
+ import store from '../store';
+
+ 
+ const letters ="ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
 </script>
