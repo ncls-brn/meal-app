a131f7b07b5e61623718dc12ce3e9bf54b178e2a
ncls-brn <nicolas.j.baran@gmail.com>
2023-03-07 13:47:05 +0100

    array mealList

--- vuejs-search-meals/src/views/Home.vue
+++ vuejs-search-meals/src/views/Home.vue
@@ -11,19 +11,24 @@
             {{ letter }}
         </router-link>
     </div>
+
+    <pre>{{ ingredients }}</pre> 
+
 </div>
     
  </template>
  
  <script setup>
- import { computed,onMounted } from 'vue';
+ import { computed,onMounted,ref } from 'vue';
  import store from '../store';
- import axiosCient from '../axiosClient.js';
+ import axiosClient from '../axiosClient.js';
 
  const letters ="ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+ const ingredients = ref([])
 
  onMounted(async () =>{
-     const response = await axiosCient.get('list.php?i=list')
+     const response = await axiosClient.get('/list.php?i=list')
      console.log(response.data)
+     ingredients.value = response.data
  })
-</script> 
+</script>  
--- vuejs-search-meals/src/views/MealList.vue
+++ vuejs-search-meals/src/views/MealList.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-    Meal List
+    Meal
 </div>
 </template>
 
