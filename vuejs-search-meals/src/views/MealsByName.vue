93b0ab8d5508959af9aa3afbb1ddfff6871f19b2
ncls-brn <nicolas.j.baran@gmail.com>
2023-03-09 13:45:11 +0100

    meals by name function search

--- vuejs-search-meals/src/views/MealsByName.vue
+++ vuejs-search-meals/src/views/MealsByName.vue
@@ -1,8 +1,20 @@
 <template>
-<div>
-    By Name
+<div class="p-10">
+    <input type= "text" 
+        v-model = "keyword"
+        class="rounded border-2 border-gray-200 w-full"
+        placeholder="Search for meals"
+        @change="searchMeals"
+    />
 </div>
 </template>
 
 <script setup>
+
+const keyword = ref('');
+function searchMeals(){
+
+};
+
+
 </script> 
