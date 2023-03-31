d33dd0a9047028b13fc7725162e7990e7ba2f0d0
ncls-brn <nicolas.j.baran@gmail.com>
2023-03-31 12:21:30 +0200

    update meals by ingredients

--- vuejs-search-meals/src/views/MealsByIngredient.vue
+++ vuejs-search-meals/src/views/MealsByIngredient.vue
@@ -1,8 +1,27 @@
 <template>
-<div>
-    By Ingredients
+<div class="p-8">
+    <h1 class="text 2xl font-bold mb-5">Ingredients</h1>
+    <router-link :to="{name:'byIngredient', param:{ingredient: ingredient.idIngredient}}"
+    
+    v-for="ingredient of ingredients" :key="ingredient.id"
+    class="block bg-white rounded p-3 mb-3 shadow">
+        <h3 class="text 2xl font-bold mb-3">{{ ingredient.strIngredient }}</h3>
+        <p>{{ ingredient.strDescription }}</p>
+</router-link>
 </div>
 </template>
 
 <script setup>
+import { onMounted, ref } from 'vue';
+import axiosClient from '../axiosClient';
+
+const ingredients =ref([])
+
+onMounted(()=>{
+    axiosClient.get('list.php?i=list').then(({data}) => {
+        
+        ingredients.value=data.meals
+
+    })
+})
 </script> 
